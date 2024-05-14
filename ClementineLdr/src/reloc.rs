b52ebcc4d96ef7294fa123eb28b60faed3025da7
Andrei <andreiviczian@protonmail.com>
2024-05-14 20:01:54 +0300

    Added more reloc type branches.

--- ClementineLdr/src/reloc.rs
+++ ClementineLdr/src/reloc.rs
@@ -1,8 +1,8 @@
 use core::mem::size_of;
 
 #[link_section = ".text"]
-pub unsafe fn fix_reloc(data_directory: IMAGE_DATA_DIRECTORY, base_address: usize, relocation_address: usize) -> bool {
-    let mut image_base_relocation = (base_address + data_directory.VirtualAddress as usize) as *mut IMAGE_BASE_RELOCATION;
+pub unsafe fn fix_reloc(data_directory: *const IMAGE_DATA_DIRECTORY, base_address: usize, relocation_address: usize) -> bool {
+    let mut image_base_relocation = (base_address + (*data_directory).VirtualAddress as usize) as *mut IMAGE_BASE_RELOCATION;
     let delta: usize = base_address - relocation_address;
 
     while (*image_base_relocation).VirtualAddress {
@@ -16,7 +16,7 @@ pub unsafe fn fix_reloc(data_directory: IMAGE_DATA_DIRECTORY, base_address: usiz
                 10 => *((base_address + (*image_base_relocation).VirtualAddress as usize + (*base_relocation_entry).Offset as usize) as *mut u64) as usize += delta,
                 3 => *((base_address + (*image_base_relocation).VirtualAddress as usize + (*base_relocation_entry).Offset as usize) as *mut u32) as usize += delta >> 16,
                 1 => *((base_address + (*image_base_relocation).VirtualAddress as usize + (*base_relocation_entry).Offset as usize) as *mut u16) as usize += delta,
-
+                0 => (),
                 _ => { return false;}
             }
 
@@ -38,13 +38,13 @@ pub struct IMAGE_DATA_DIRECTORY {
 #[repr(C)]
 pub struct IMAGE_BASE_RELOCATION
 {
-    pub VirtualAddress: u32, // Where the relocation must be applied
+    pub VirtualAddress: u32, // Page where the relocations will be performed
     pub SizeOfBlock: u32
 }
 
 #[repr(C)]
 pub struct BASE_RELOCATION_ENTRY
 {
-    pub Offset: u32,
+    pub Offset: u32, // Offset to the pointer who will be relocated
     pub Type: u32
 }
